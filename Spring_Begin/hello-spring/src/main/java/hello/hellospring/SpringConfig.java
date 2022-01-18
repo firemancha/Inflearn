225372c9788ac35ea973a12ef5ac2eaf26c0c02f
firemanCha <vaite714@gmail.com>
2022-01-18 21:48:13 +0900

    AOP

--- Spring_Begin/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring_Begin/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,11 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import javax.swing.*;
 
 @Configuration
 public class SpringConfig {
@@ -35,6 +32,11 @@ public class SpringConfig {
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository);
+	}
+
+	@Bean
+	public TimeTraceAop timeTraceAop() {
+		return new TimeTraceAop();
 	}
 
 //	@Bean
