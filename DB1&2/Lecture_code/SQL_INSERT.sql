aabd1b84d939ce985984eb856fe722531821aee2
firemanCha <vaite714@gmail.com>
2022-01-18 19:31:46 +0900

    MySQL CRUD

--- DB1&2/Lecture_code/SQL_INSERT.sql
+++ DB1&2/Lecture_code/SQL_INSERT.sql
@@ -1,7 +1,50 @@
+/* CREATE(Crud)
+  INSERT [LOW_PRIORITY | DELAYED | HIGH_PRIORITY] [IGNORE]
+    [INTO] tbl_name
+    [PARTITION (partition_name [, partition_name] ...)]
+    [(col_name [, col_name] ...)]
+    { {VALUES | VALUE} (value_list) [,(value_list)]
+      |
+      VALUES row_contructor_list
+    }
+    [AS row_alias[(col_alias [, col_alias] ...)]]
+    [ON DUPLICATE KEY UPDATE assignment_list]
+
+  INSERT [LOW_PRIORITY | DELAYED | HIGH_PRIORITY] [IGNORE]
+    [INTO] tbl_name
+    [PARTITION (partition_name [, partition_name] ...)]
+    [AS row_alias[(col_alias [, col_alias] ...)]]
+    SET assignment_list
+    [ON DUPLICATE KEY UPDATE assignment_list]
+
+  INSERT [LOW_PRIORITY | HIGH_PRIORITY] [IGNORE]
+    [INTO] tbl_name
+    [PARTITION (partition_name [,partition_name] ...)]
+    [(col_name [, col_name] ...)]
+    [AS row_alias[(col_alias [, col_alias] ...)]]
+    {SELECT ... | TABLE table_name}
+    [ON DUPLICATE KEY UPDATE assignment_ list]
+
+  value:
+    {expr | DEFAULT}
+
+  value_list:
+    value [, value]
+  
+  row_constructor_list:
+    ROW(value_list)[, ROW(value_list)][, ...]
+  
+  assignment:
+    col_name = [row_alias.]value
+  
+  assignment_list:
+    assignment [,assignment] ...
+*/
+
 /* TABLE의 구조 출력 */
 DESC topic;
 
-/* CREATE(Crud)
+/*
   id: 자동 추가
   title: MySQL
   description: MySQL is ...
@@ -13,9 +56,7 @@ DESC topic;
 /* NOW(): 현재 시간 */
 INSERT INTO topic(title, description, created, author, profile) VALUES('MySQL', 'MySQL is ...', NOW(), 'egoing', 'developer');
 
-/* READ(cRud)
-  topic TABLE로부터 모든 데이터를 가져옴
-*/
+/* topic TABLE로부터 모든 데이터를 가져옴 */
 SELECT * from topic;
 
 /* 정보 추가 */
--- DB1&2/Lecture_code/SQL_UPDATE.sql
+++ DB1&2/Lecture_code/SQL_UPDATE.sql
@@ -1,6 +1,18 @@
 /* UPDATE(crUd)
-  UPDATE (TABLE) SET assignment [WHERE] [ORDER BY] [LIMIT];
-  assingmet: COLUMN = VALUE
+  UPDATE [LOW_PRIORITY] [IGNORE] table_reference
+    SET assignment_list
+    [WHERE where_condition]
+    [ORDER BY ...]
+    [LIMIT row_count]
+  
+  value:
+    {expr | DEFAULT}
+  
+  assignment:
+    col_name = value
+  
+  assignment_list:
+    assignment [, assignment] ...
 */
 
 /* topic TABLE의 id값이 2인 행의 title을 Oracle로, description을 Oracle is ...로 변경*/
