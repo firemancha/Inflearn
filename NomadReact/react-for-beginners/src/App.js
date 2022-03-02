6c1565ed99feb5b2be6cdb2b9b303b72b161e722
firemancha <vaite714@gmail.com>
2022-03-02 21:58:02 +0900

    Movie App part Two

--- NomadReact/react-for-beginners/src/App.js
+++ NomadReact/react-for-beginners/src/App.js
@@ -1,39 +1,39 @@
 import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [dollar, setDollar] = useState("");
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  const onChange = (event) => setDollar(event.target.value);
+
   return (
     <div>
-      <h1>The Coins!({coins.length})</h1>
-      <label htmlFor="dollar">Dollar </label>
-      <input
-        id="dollar"
-        onChange={onChange}
-        type="number"
-        value={dollar}
-        placeholder="Write dollar you have..."
-      />
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <ul>
-          {coins.map((coin) => (
-            <li>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD :{"\t"}
-              {dollar / coin.quotes.USD.price} EA
-            </li>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title} />
+              <h2>{movie.title_long}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
