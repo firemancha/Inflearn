3b71b0297abf7e5f8fadb96ca08078a53c55cd23
firemancha <vaite714@gmail.com>
2022-03-02 22:54:00 +0900

    Parameters

--- NomadReact/react-for-beginners/src/routes/Detail.js
+++ NomadReact/react-for-beginners/src/routes/Detail.js
@@ -1,5 +1,53 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt="" />
+          <h1>{movie.title}</h1>
+          <h3>Description</h3>
+          <p>{movie.description_full}</p>
+          <h3>Info</h3>
+          <div>
+            <span style={{ margin: "20px" }}>
+              <b>{movie.year}</b>year
+            </span>
+            <span style={{ margin: "20px" }}>
+              <b>{movie.rating}</b>rating
+            </span>
+            <span style={{ margin: "20px" }}>
+              <b>runtime: </b>
+              {Math.floor(movie.runtime / 60)} H {movie.runtime % 60} M
+            </span>
+          </div>
+          <h3>Genres</h3>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
