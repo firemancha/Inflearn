ab519b85f104ff4a327bc278f36096b8000b2a98
firemancha <vaite714@gmail.com>
2022-03-02 20:03:53 +0900

    Movie App part One

--- NomadReact/react-for-beginners/src/App.js
+++ NomadReact/react-for-beginners/src/App.js
@@ -1,37 +1,40 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [dollar, setDollar] = useState("");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const onChange = (event) => setDollar(event.target.value);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins!({coins.length})</h1>
+      <label htmlFor="dollar">Dollar </label>
+      <input
+        id="dollar"
+        onChange={onChange}
+        type="number"
+        value={dollar}
+        placeholder="Write dollar you have..."
+      />
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD :{"\t"}
+              {dollar / coin.quotes.USD.price} EA
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
