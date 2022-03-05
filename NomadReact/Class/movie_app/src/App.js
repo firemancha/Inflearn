91d8eba58641823286123264411ade2b8fef01b3
firemancha <vaite714@gmail.com>
2022-03-05 17:54:12 +0900

    Planning the Movie Component

--- NomadReact/Class/movie_app/src/App.js
+++ NomadReact/Class/movie_app/src/App.js
@@ -1,44 +1,19 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-
   state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
+    isLoading: true,
   };
 
   componentDidMount() {
-    console.log("component rendered");
-  }
-
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-
-  componentWillUnmount() {
-    console.log("Goodbye, creul world");
+    setTimeout(() => {
+      this.setState((current) => (current.isLoading = false));
+    }, 6000);
   }
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
