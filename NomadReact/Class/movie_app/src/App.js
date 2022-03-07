b7de7b7c9db1d8968e42ef5f67470fbdbf5154f4
firemancha <vaite714@gmail.com>
2022-03-07 17:39:57 +0900

    Building the Navigation

--- NomadReact/Class/movie_app/src/App.js
+++ NomadReact/Class/movie_app/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- NomadReact/Class/movie_app/src/routes/About.js
+++ NomadReact/Class/movie_app/src/routes/About.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <h1>About this page: I built it beacause I love movies.</h1>;
+  return (
+    <div className="about_container">
+      <span>
+        “Freedom is the freedom to say that two plus two make four. If that is
+        granted, all else follows.”
+      </span>
+      <span>- George Orwell, 1984</span>
+    </div>
+  );
 }
 
 export default About;
