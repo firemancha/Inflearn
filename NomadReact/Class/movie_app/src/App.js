78dfeebc0ab91516fa654e4a3f2d0fba002acb52
firemancha <vaite714@gmail.com>
2022-03-07 17:28:40 +0900

    Building the Router

--- NomadReact/Class/movie_app/src/App.js
+++ NomadReact/Class/movie_app/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
-  return <span>somthing</span>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ NomadReact/Class/movie_app/src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <h1>About this page: I built it beacause I love movies.</h1>;
+}
+
+export default About;
